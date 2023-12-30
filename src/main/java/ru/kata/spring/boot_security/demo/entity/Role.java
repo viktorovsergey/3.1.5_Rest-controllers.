d9c15614da27270169f7f9656e1dd99456714239
ya_zabiyaka <ya_zabiyaka@bk.ru>
2023-12-30 17:01:58 +0300

    2 типа что то куда то выводит

---
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
@@ -0,0 +1,54 @@
+package ru.kata.spring.boot_security.demo.entity;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    public Role() {
+    }
+
+    public Role(String roleName, List<User> users) {
+        this.roleName = roleName;
+        this.users = users;
+    }
+
+    public Role(String roleName) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
@@ -0,0 +1,82 @@
+package ru.kata.spring.boot_security.demo.entity;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String firstName;
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String lastName;
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String email, String lastName, List<Role> roles) {
+        this.id = id;
+        this.firstName = firstName;
+        this.email = email;
+        this.lastName = lastName;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+//    public String getStringRoles() {
+//        return getRoles().stream().map(Role::getRoleName).collect(Collectors.joining(";  "));
+//    }
+}
